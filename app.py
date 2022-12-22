4535c79f146f9e9301d3873f52a1297731695279
adlinaf <121245742+adlinaf@users.noreply.github.com>
2022-12-23 01:25:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import base64
 from PIL import Image, ImageOps
 from tensorflow import keras
 from keras.preprocessing import image
@@ -12,21 +11,7 @@ st.header("1. To investigate Malaysian banknotes differences and features.")
 st.header("1. To investigate Malaysian banknotes differences and features.")
 
 st.text("Upload a banknote for image classification to identify what banknote it is")
-def add_bg_from_local(image_file):
-    with open(image_file, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read())
-    st.markdown(
-    f"""
-    <style>
-    .stApp {{
-        background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
-        background-size: cover
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-    )
-add_bg_from_local('sky.jpg') 
+
 
 uploaded_file = st.file_uploader("Upload a banknote image...", type="jpg")
 if uploaded_file is not None:
